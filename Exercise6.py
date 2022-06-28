4d1f3acffa684125bbae3218538c8a5ea5eab0cd
Aiden300 <47370726+Aiden300@users.noreply.github.com>
2022-06-28 15:55:27 -0700

    Update Exercise6.py

--- Exercise6.py
+++ Exercise6.py
@@ -5,13 +5,23 @@
 # returning: print statements to console to inform user if the original_list is a palindrome
 # or not a palindrome
 # Note: A palindrome is a string that reads the same forwards and backwards.
-def palindrome_function(original_list, user_string):
-    # reversing the list
-    reversed_list = original_list[::-1]
+def palindrome_function(user_string):
 
-    # comparing reversed_list to original_list to
-    # determine if the original_list is a palindrome
-    if reversed_list == original_list:
+    # 'user_list_no_whitespaces' is a list that
+    # holds the same value as 'user_string' but with no
+    # whitespaces and lower case
+    original_list_no_whitespaces = []
+    for ele in user_string.lower():
+        if ele.strip():
+            original_list_no_whitespaces.append(ele)
+
+    # 'reversed_list' holds the reversed version of
+    # 'original_list_no_whitespaces'
+    reversed_list = original_list_no_whitespaces[::-1]
+
+    # comparing reversed_list to original_list_no_whitespaces to
+    # determine if the user_string is a palindrome
+    if reversed_list == original_list_no_whitespaces:
         print("'" + user_string.lower() + "' is a palindrome.")
     else:
         print("'" + user_string.lower() + "' is not a palindrome!")
@@ -22,17 +32,16 @@ def main():
     print("enter 'exit' to end program")
     print()
 
+    # prompting user for input
     user_string = input("Enter string: ")
-    user_list = list(user_string.lower())
     while user_string != 'exit':
+
         # putting our palindrome_function() to use
-        palindrome_function(user_list, user_string)
+        palindrome_function(user_string)
         print()
+
+        # prompting user for input
         user_string = input("Enter string: ")
-        # creating a variable called user_list
-        # to hold the list and .lower() version
-        # of user_string
-        user_list = list(user_string.lower())
 
     print("Goodbye!")
 
