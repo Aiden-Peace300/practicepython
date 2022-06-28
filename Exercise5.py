a72d44c4acc817de651a9337b4445e6cd28fdb7a
Aiden300 <47370726+Aiden300@users.noreply.github.com>
2022-06-28 01:33:51 -0700

    Update Exercise5.py

--- Exercise5.py
+++ Exercise5.py
@@ -1,5 +1,7 @@
 #! /usr/bin/env python3
 
+import random   # used to generate random numbers
+
 
 # finding_commonalities_function: this function will take in two list as a parameter
 # returning: print statements to console to inform user of the elements that
@@ -12,24 +14,40 @@ def finding_commonalities_function(list1, list2):
     # set() method is used to convert any of the iterable to sequence of
     # iterable elements with distinct elements, commonly called Set.
     # The list() function creates a list object.
-    print(list(set(list1).intersection(list2)))
+    print(set(list1).intersection(list2))
+
+
+# generating_list_function: this function will take in two list as a parameter
+# returning: This will output nothing.
+# goal: to create put randomly generated numbers into the empty list passed in
+def generating_list_function(list1, list2):
+    for i in range(0, 11):
+        n = random.randint(1, 100)
+        list1.append(n)
+
+    for i in range(0, 13):
+        n = random.randint(1, 100)
+        list2.append(n)
 
 
 def main():
 
-    # Given list
-    a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-    b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+    # Creating/generating random list
+    random_list1 = []
+    random_list2 = []
+
+    # using generating_list_function()
+    generating_list_function(random_list1, random_list2)
 
     # outputting given list(s) to console
-    print("Given List:")
-    print(a)
-    print(b)
+    print("Random generated List:")
+    print(random_list1)
+    print(random_list2)
     print()
 
     # outputting commonalities and using finding_commonalities_function()
     print("Commonalities between the two list w/ no duplicates: ")
-    finding_commonalities_function(a, b)
+    finding_commonalities_function(random_list1, random_list2)
 
 
 if __name__ == "__main__":
