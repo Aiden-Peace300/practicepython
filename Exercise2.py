52aa7c000fb44e93c8a19c75cce7fb8d4d8427d5
Aiden300 <47370726+Aiden300@users.noreply.github.com>
2022-07-08 22:09:26 -0700

    Update Exercise2.py

--- Exercise2.py
+++ Exercise2.py
@@ -3,7 +3,7 @@
 """
 PROGRAMMER: Aiden Peace
 DATE: 6/24/2022
-TITLE: Exercise 2 : Odd Or Even
+TITLE: Exercise 2 - Odd Or Even
 DESCRIPTION: Ask the user for a number. Depending on whether
 the number is even or odd, print out an appropriate message
 to the user.
