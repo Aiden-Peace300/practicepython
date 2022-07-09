85cc2bfb553134e3f0f2619126de439ff9f53fd8
Aiden300 <47370726+Aiden300@users.noreply.github.com>
2022-07-08 22:08:48 -0700

    Update Exercise2.py

--- Exercise2.py
+++ Exercise2.py
@@ -3,11 +3,17 @@
 """
 PROGRAMMER: Aiden Peace
 DATE: 6/24/2022
-TITLE: Exercise 1 : Guessing Game One
-DESCRIPTION: Create a program that asks the user
-to enter their name and their age. Print out a
-message addressed to them that tells them the year
-that they will turn 100 years old.
+TITLE: Exercise 2 : Odd Or Even
+DESCRIPTION: Ask the user for a number. Depending on whether
+the number is even or odd, print out an appropriate message
+to the user.
+
+Extras:
+1. If the number is a multiple of 4, print out a different message.
+2. Ask the user for two numbers: one number to check (call it num)
+   and one number to divide by (check). If check divides evenly into
+   num, tell that to the user. If not, print a different appropriate
+   message.
 """
 
 
