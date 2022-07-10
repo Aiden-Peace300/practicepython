002f9ec94ecd73814adf465c7a6967d36d910e29
Aiden300 <47370726+Aiden300@users.noreply.github.com>
2022-07-10 15:09:00 -0700

    Update Exercise5.py

--- Exercise5.py
+++ Exercise5.py
@@ -2,13 +2,37 @@
 
 import random   # used to generate random numbers
 
+"""
+PROGRAMMER: Aiden Peace
+DATE: 6/27/2022
+TITLE: Exercise 4 - Divisors
+DESCRIPTION: Take two lists, say for example these two:
 
-# finding_commonalities_function: this function will take in two list as a parameter
-# returning: print statements to console to inform user of the elements that
-# are similarly/commonalities with the two list pass in keep in mind. This
-# function will not output duplicates.
+  a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+  b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+  
+And write a program that returns a list that contains only the 
+elements that are common between the lists (without duplicates). 
+Make sure your program works on two lists of different sizes.
+
+Extras:
+Randomly generate two lists to test this
+Write this in one line of Python (don’t worry if you can’t figure this out at this point - we’ll get to it soon)
+"""
+
+
 def finding_commonalities_function(list1, list2):
+    """
+    FUNCTION DESCRIPTION:
 
+    GOAL  : This function will find commonalities with no duplicates
+            and return these values to console.
+    INPUT : This function will take in
+            two list as a parameter.
+    OUTPUT:  print statements to console to inform user of the elements that
+             are similarly/commonalities with the two list pass in keep in mind. This
+             function will not output duplicates.
+    """
     # closer look:
     # intersection() allows arbitrary number of arguments (sets).
     # set() method is used to convert any of the iterable to sequence of
@@ -21,6 +45,16 @@ def finding_commonalities_function(list1, list2):
 # returning: This will output nothing.
 # goal: to create randomly generated numbers into the empty list(s) passed in
 def generating_list_function(list1, list2):
+    """
+    FUNCTION DESCRIPTION:
+
+    GOAL  : This function will find commonalities with no duplicates
+            and return these values to console.
+    INPUT : This function will take in
+            two list as a parameter (preferable empty lists)
+    OUTPUT:  This will output nothing.
+    """
+
     for i in range(0, 11):
         n = random.randint(1, 100)
         list1.append(n)
