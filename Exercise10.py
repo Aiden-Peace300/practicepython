63a9372305d30136b656a89833cac3f4e6455e09
Aiden300 <47370726+Aiden300@users.noreply.github.com>
2022-07-10 15:25:37 -0700

    Update Exercise10.py

--- Exercise10.py
+++ Exercise10.py
@@ -2,13 +2,35 @@
 
 import random   # used to generate random numbers
 
+"""
+PROGRAMMER: Aiden Peace 
+DATE: 7/6/2022
+TITLE: Exercise 10 : List Overlap Comprehensions
+DESCRIPTION: This week’s exercise is going to be revisiting an 
+old exercise (see Exercise 5), except require the solution in 
+a different way. Take two lists, say for example these two:
 
-# finding_commonalities_function: this function will take in two list as a parameter
-# returning: print statements to console to inform user of the elements that
-# are similarly/commonalities with the two list pass in keep in mind. This
-# function will not output duplicates.
+  a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+  b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+  
+and write a program that returns a list that contains only the 
+elements that are common between the lists (without duplicates). 
+Make sure your program works on two lists of different sizes.
+"""
+
+
 def finding_commonalities_function(list1, list2):
+    """
+    FUNCTION DESCRIPTION:
 
+    GOAL  : This function will find commonalities with no duplicates
+            and return these values to console.
+    INPUT : This function will take in
+            two list as a parameter.
+    OUTPUT:  print statements to console to inform user of the elements that
+             are similarly/commonalities with the two list pass in keep in mind. This
+             function will not output duplicates.
+    """
     list3 = []
     for i in list1:
         for j in list2:
@@ -18,10 +40,17 @@ def finding_commonalities_function(list1, list2):
     print(list3)
 
 
-# generating_list_function: this function will take in two list as a parameter
-# returning: This will output nothing.
-# goal: to create randomly generated numbers into the empty list(s) passed in
 def generating_list_function():
+    """
+    FUNCTION DESCRIPTION:
+
+    GOAL  : This function will find commonalities with no duplicates
+            and return these values to console.
+    INPUT : This function will take in
+            two list as a parameter (preferable empty lists)
+    OUTPUT:  This will output nothing.
+    """
+
     list1 = random.sample(range(1, 30), 12)
     list2 = random.sample(range(1, 30), 16)
     return list1, list2
