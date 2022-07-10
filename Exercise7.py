6c1f6573496f8657850cc25163c81ccca554a277
Aiden300 <47370726+Aiden300@users.noreply.github.com>
2022-07-10 15:16:21 -0700

    Update Exercise7.py

--- Exercise7.py
+++ Exercise7.py
@@ -1,8 +1,16 @@
 #! /usr/bin/env python3
 
-# EXERCISE 7 : Let’s say I give you a list saved in a variable: a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100].
-# Write one line of Python that takes this list a and makes a new list that has only the even elements 
-# of this list in it.
+"""
+PROGRAMMER: Aiden Peace
+DATE: 6/28/2022
+TITLE: Exercise 7 - List Comprehensions
+DESCRIPTION: Let’s say I give you a list saved
+in a variable: a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100].
+Write one line of Python that takes this list a and makes
+a new list that has only the even elements of this list in it.
+"""
+
+
 def main():
     # Given list
     a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
